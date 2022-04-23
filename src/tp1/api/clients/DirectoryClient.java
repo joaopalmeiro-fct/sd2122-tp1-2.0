e5997e723b685957e4d7d38101de2c2d7ec353e3
Tiago Alexandre Portugal Morais <tiago@Air-de-Tiago.Home>
2022-04-23 21:12:55 +0100

    Directories rest client e interface

--- src/tp1/api/clients/DirectoryClient.java
+++ src/tp1/api/clients/DirectoryClient.java
@@ -1,5 +1,18 @@
 package tp1.api.clients;
 
+import tp1.api.service.util.Result;
+
 public interface DirectoryClient {
+	
+	/**
+	 * Deletes all files of the given user, removes it from any sharings and deletes all info associated with it.
+	 * @param userId - id of the user.
+	 * @param password - the password of the user.
+	 * @return OK if success (regardless of having any files). 
+	 *		   NOT_FOUND if the userId does not exist.
+	 *         FORBIDDEN if the password is incorrect.
+	 * 		   BAD_REQUEST otherwise.
+	 */
+	Result<Void> deleteAllFiles(String userId, String password);
 
 }
--- src/tp1/clients/rest/RestDirectoryClient.java
+++ src/tp1/clients/rest/RestDirectoryClient.java
@@ -5,12 +5,17 @@ import java.util.logging.Logger;
 
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import tp1.api.clients.DirectoryClient;
+import tp1.api.clients.FilesClient;
 import tp1.api.clients.RestClient;
 import tp1.api.service.rest.RestDirectory;
 import tp1.api.service.rest.RestFiles;
 import tp1.api.service.rest.RestUsers;
+import tp1.api.service.util.Result;
+import tp1.api.service.util.Result.ErrorCode;
 
-public class RestDirectoryClient extends RestClient {
+public class RestDirectoryClient extends RestClient implements DirectoryClient {
 
 	//private static final String DELETEALL_PATH = "deleteall/";
 	WebTarget target;
@@ -35,7 +40,8 @@ public class RestDirectoryClient extends RestClient {
 		System.setProperty("java.net.preferIPv4Stack", "true");
 	}
 
-	public Response deleteFiles(String userId, String password) {
+	@Override
+	public Result<Void> deleteAllFiles(String userId, String password) {
 		return super.reTry(() -> {
 			return clt_deleteFiles(userId, password);
 		});
@@ -43,12 +49,17 @@ public class RestDirectoryClient extends RestClient {
 
 //------------------------------------------------------------------------------------------------
 
-	private Response clt_deleteFiles(String userId, String password) {
+	private Result<Void> clt_deleteFiles(String userId, String password) {
 
 		Response r = target.path(RestDirectory.DELETEALL_PATH + userId)
 				.queryParam(RestUsers.PASSWORD, password)
 				.request().delete();
-
-		return r;
+		
+		int status = r.getStatus();
+		
+		if (status == Status.NO_CONTENT.getStatusCode())
+			return Result.ok();
+		else
+			return Result.error(ErrorCode.valueOf((Status.fromStatusCode(status).toString())));
 	}
 }
