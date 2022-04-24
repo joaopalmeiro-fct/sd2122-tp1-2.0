fea670026b2831d90025b60bb1d9d222c2ce04c9
tmorais-fctunl <t.morais@campus.fct.unl.pt>
2022-04-24 01:13:34 +0100

    directoryResource

--- src/tp1/service/rest/DirectoryResource.java
+++ src/tp1/service/rest/DirectoryResource.java
@@ -2,6 +2,8 @@ package tp1.service.rest;
 
 import java.util.List;
 
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
 import tp1.api.FileInfo;
 import tp1.api.service.rest.RestDirectory;
 import tp1.api.service.util.Directory;
@@ -10,51 +12,82 @@ import tp1.service.util.JavaDirectory;
 
 public class DirectoryResource implements RestDirectory{
 	
-	final Directory service;
+	final Directory directoryImpl;
 	
 	public DirectoryResource(Discovery discovery) {
-		service = new JavaDirectory(discovery);
+		directoryImpl = new JavaDirectory(discovery);
 	}
 
 	@Override
 	public FileInfo writeFile(String filename, byte[] data, String userId, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		var result = directoryImpl.writeFile(filename, data, userId, password);
+
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 	}
 
 	@Override
 	public void deleteFile(String filename, String userId, String password) {
-		// TODO Auto-generated method stub
+		var result = directoryImpl.deleteFile(filename, userId, password);
+
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
 	@Override
 	public void shareFile(String filename, String userId, String userIdShare, String password) {
-		// TODO Auto-generated method stub
+		var result = directoryImpl.shareFile(filename, userId, userIdShare, password);
+
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
 	@Override
 	public void unshareFile(String filename, String userId, String userIdShare, String password) {
-		// TODO Auto-generated method stub
+		var result = directoryImpl.unshareFile(filename, userId, userIdShare, password);
+
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
 	@Override
 	public byte[] getFile(String filename, String userId, String accUserId, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		var result = directoryImpl.getFile(filename, userId, accUserId, password);
+
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 	}
 
 	@Override
 	public List<FileInfo> lsFile(String userId, String password) {
-		// TODO Auto-generated method stub
-		return null;
+		var result = directoryImpl.lsFile(userId, password);
+
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 	}
 
 	@Override
 	public void deleteAllFiles(String userId, String password) {
-		// TODO Auto-generated method stub
+		var result = directoryImpl.deleteAllFiles(userId, password);
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
