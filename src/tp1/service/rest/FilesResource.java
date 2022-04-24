8a1fcb05331717cfadb5574fa90052fa2c987a54
joaopalmeiro-fct <j.palmeiro@campus.fct.unl.pt>
2022-04-24 01:13:41 +0100

    FilesResource

--- src/tp1/service/rest/FilesResource.java
+++ src/tp1/service/rest/FilesResource.java
@@ -1,40 +1,72 @@
 package tp1.service.rest;
 
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
 import tp1.api.service.rest.RestFiles;
 import tp1.api.service.util.Files;
+import tp1.api.service.util.Users;
 import tp1.discovery.Discovery;
 import tp1.service.util.JavaFiles;
+import tp1.service.util.JavaUsers;
 
 public class FilesResource implements RestFiles {
 	
-	final Files service;
-	
+	Discovery discovery;
+
+	Files filesImpl;
+
 	public FilesResource (Discovery discovery) {
-		service = new JavaFiles(discovery);
+
+		this.filesImpl = new JavaFiles(discovery);
+
+		this.discovery = discovery;
+
 	}
 
 	@Override
 	public void writeFile(String fileId, byte[] data, String token) {
-		// TODO Auto-generated method stub
+		
+		var result = filesImpl.writeFile(fileId, data, token);
+
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
 	@Override
 	public void deleteFile(String fileId, String token) {
-		// TODO Auto-generated method stub
+		
+		var result = filesImpl.deleteFile(fileId, token);
+
+		if (result.isOK())
+			return;
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 		
 	}
 
 	@Override
 	public byte[] getFile(String fileId, String token) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		var result = filesImpl.getFile(fileId, token);
+
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 	}
 
 	@Override
 	public Integer deleteAllFiles(String userId, String token) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		var result = filesImpl.deleteAllFiles(userId, token);
+
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(Status.fromStatusCode(result.getErrorCodeNum()));
 	}
 
 }
